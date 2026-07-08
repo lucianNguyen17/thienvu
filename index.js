15622fc729eb5a63b0623188efd2452587797cc3
Nam <llsaksers2@gmail.com>
2026-07-08 07:59:47 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan'); // Khai báo morgan
+
+const app = express();
+const port = 3000;
+
+// Tích hợp morgan vào app
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.send('Chào mừng đến với Blog cá nhân của tôi!');
+});
+
+app.listen(port, () => {
+  console.log(`Server đang chạy tại http://localhost:${port}`);
+});
